28b4f129039c712e4d2861758b1361ea20ae70e9
Alexander Kahn <alexanderkahn@gmail.com>
2011-06-06 16:07:12 -0400

    Make the extension configurable

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-var ticketUrl = 'https://paperlesspost.unfuddle.com/a#/projects/2/tickets/by_number/',
-    ticketRegexp = /\#(\d+)/;
+chrome.extension.sendRequest({method: "getConfig"}, function(response) {
+  console.log('running');
+  if (window.location.match(new RegExp('https://github.com/.+/' + response.repoName + '/commit/.+'))) {
+    var ticketUrl = response.ticketUrl,
+        ticketRegexp = /\#(\d+)/;
 
-$('.commit .human pre').each(function(i, elem) {
-  var commit = $(elem),
-      message = commit.text();
+    $('.commit .human pre').each(function(i, elem) {
+      var commit = $(elem),
+          message = commit.text();
 
-  if (message.match(ticketRegexp)) {
-    message = message.replace(ticketRegexp, function(match) {
-      var link = $('<div><a></a></div>');
+      if (message.match(ticketRegexp)) {
+        message = message.replace(ticketRegexp, function(match) {
+          var link = $('<div><a></a></div>');
 
-      link
-        .find('a')
-        .css('color', '#4133C4')
-        .attr('href', ticketUrl + match.substring(1))
-        .text(match);
+          link
+            .find('a')
+            .css('color', '#4133C4')
+            .attr('href', ticketUrl.replace('###', match.substring(1)))
+            .text(match);
 
-      return link.html();
+          return link.html();
+        });
+
+        commit.html(message);
+      }
     });
-
-    commit.html(message);
   }
 });
